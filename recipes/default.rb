97108b526b685caee5e5fa322bfbc18afc56647a
Seth Floyd <seth@sethfloydjr.com>
2016-12-14 17:10:29 -0500

    updating

--- recipes/default.rb
+++ recipes/default.rb
@@ -9,24 +9,15 @@
 
 
 
-if platform_family?('rhel')
+#install new repo
+execute 'postgres95_yum' do
+	command 'rpm -Uvh https://download.postgresql.org/pub/repos/yum/9.5/redhat/rhel-6-x86_64/pgdg-ami201503-95-9.5-3.noarch.rpm'
+	action :run
+end
 
-	#install new repo
-	execute 'postgres95_yum' do
-		command 'rpm -Uvh https://download.postgresql.org/pub/repos/yum/9.5/redhat/rhel-6-x86_64/pgdg-ami201503-95-9.5-3.noarch.rpm'
-	end
-	#install postgres95 from new repo
-	execute 'postgres95_install_rhel' do
-		command 'yum -y install postgres95'
-	end
 
-else
-	platform_family?('debian')
-		execute 'postgres95_apt'
-			command 'sh -c 'echo "deb http://apt.postgresql.org/pub/repos/apt/ `lsb_release -cs`-pgdg main" >> /etc/apt/sources.list.d/pgdg.list''
-		end
-		execute'postgres95_install_debian'
-			command 'wget -q https://www.postgresql.org/media/keys/ACCC4CF8.asc -O - | sudo apt-key add -'
-		end
-
+#install postgres95 from new repo
+execute 'postgres95_install_rhel' do
+	command 'yum -y install postgres95'
+	action :run
 end
