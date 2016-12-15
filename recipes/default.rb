5a336e0191d918cdaec9d646bdbfe20546379ccd
Seth Floyd <seth@sethfloydjr.com>
2016-12-14 20:11:34 -0500

    updating

--- recipes/default.rb
+++ recipes/default.rb
@@ -10,8 +10,11 @@
 
 
 #install postgres95 from psql repo
-package 'postgres95' do
-	action :install
-	source 'https://download.postgresql.org/pub/repos/yum/9.5/redhat/rhel-6-x86_64/pgdg-ami201503-95-9.5-3.noarch.rpm'
-	provider Chef::Provider::Package::Rpm
+execute 'postgres95_yum' do
+	command 'rpm -Uvh https://download.postgresql.org/pub/repos/yum/9.5/redhat/rhel-6-x86_64/pgdg-ami201503-95-9.5-3.noarch.rpm'
+end
+
+#install postgres95 from new repo
+execute 'postgres95_install_rhel' do
+	command 'yum -y install postgres95'
 end
